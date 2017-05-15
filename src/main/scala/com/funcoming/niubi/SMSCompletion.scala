b587259ce1030c6c056c5370c661ecb9c8571755
Rocke <909104374@qq.com>
2017-05-15 15:36:02 +0800

    添加本地调试模式

--- src/main/scala/com/funcoming/niubi/SMSCompletion.scala
+++ src/main/scala/com/funcoming/niubi/SMSCompletion.scala
@@ -108,12 +108,14 @@ object SMSCompletion {
     //    val location = MainApp.getLocation(sc, imsi)
     val md5Str = MainApp.getLineMD5(line)
     val businessCode = getBusinessCode(bc, content)
+    println("业务码是" + businessCode)
     val status = getStatus(businessCode, content)
     return line + "|" + businessCode + "|" + md5Str + "|" + status
   }
 
   def start(inputpath: String, outputpaath: String): Unit = {
-    val conf = new SparkConf().setAppName("短信附加信息添加")
+//    val conf = new SparkConf().setAppName("短信附加信息添加")
+    val conf = new SparkConf().setAppName("短信附加信息添加").setMaster("local[*]")
     val sparkContext = new SparkContext(conf)
     val bc = sparkContext.broadcast(getReferenceMap())
     val lnc = sparkContext.accumulator(0L, "LineNumberCounter")
