b6aa1b41544f462fb255485db32709355f50dc13
Rocke <909104374@qq.com>
2017-05-15 17:23:04 +0800

    测试通过

--- src/main/scala/com/funcoming/niubi/SMSCompletion.scala
+++ src/main/scala/com/funcoming/niubi/SMSCompletion.scala
@@ -73,7 +73,7 @@ object SMSCompletion {
     val dinggou = ("订购", "定制", "订制", "办理")
     dinggou.productIterator.foreach {
       ele => {
-        var tempIndex = contentPreSub.lastIndexOf(ele)
+        var tempIndex = contentPreSub.lastIndexOf("" + ele)
         if (tempIndex > dingMax) {
           dingMax = tempIndex
         }
@@ -108,13 +108,14 @@ object SMSCompletion {
     //    val location = MainApp.getLocation(sc, imsi)
     val md5Str = MainApp.getLineMD5(line)
     val businessCode = getBusinessCode(bc, content)
-    println("业务码是" + businessCode)
+    print("业务码是" + businessCode)
     val status = getStatus(businessCode, content)
+    println("--------" + status)
     return line + "|" + businessCode + "|" + md5Str + "|" + status
   }
 
   def start(inputpath: String, outputpaath: String): Unit = {
-//    val conf = new SparkConf().setAppName("短信附加信息添加")
+    //    val conf = new SparkConf().setAppName("短信附加信息添加")
     val conf = new SparkConf().setAppName("短信附加信息添加").setMaster("local[*]")
     val sparkContext = new SparkContext(conf)
     val bc = sparkContext.broadcast(getReferenceMap())
